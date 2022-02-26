fe393c304bb8843ad0ba00bc7ff153e9fb29bb8b
Karol (Bullas) Attekita <bullas.atk@gmail.com>
2022-02-25 23:29:12 -0300

    Start project

---
+++ Assets/Scripts/Mechanics/DeathZone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// DeathZone components mark a collider which will schedule a
+    /// PlayerEnteredDeathZone event when the player enters the trigger.
+    /// </summary>
+    public class DeathZone : MonoBehaviour
+    {
+        private Rigidbody2D rigidBody;
+        private float direction = 1;
+        public float velocity = 1f;
+
+        private void Start()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update () {
+            rigidBody.velocity = new Vector2(0f, velocity * direction);
+        }
+
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            var p = collider.gameObject.GetComponent<PlayerController>();
+            if (p != null)
+            {
+                var ev = Schedule<PlayerEnteredDeathZone>();
+                ev.deathzone = this;
+            }
+        }
+    }
+}
