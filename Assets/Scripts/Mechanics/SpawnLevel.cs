faf22e92f698ecbebcd965f8e485dec4516e26ab
Karol (Bullas) Attekita <bullas.atk@gmail.com>
2022-05-31 21:07:36 -0300

    Fix tileset collider

--- Assets/Scripts/Mechanics/SpawnLevel.cs
+++ Assets/Scripts/Mechanics/SpawnLevel.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Platformer.Mechanics
 {
@@ -9,14 +10,43 @@ namespace Platformer.Mechanics
     {
 
         public GameObject[] levelPrefabs;
+        public LevelController firstLevel;
         [SerializeField]
         private float levelHeight = 12.5f;
+        private LinkedList<LevelController> levels = new LinkedList<LevelController>();
+        private int offsetToClean = 3;
+        private bool needToDestroy => offsetToClean == 0;
+
+        private void Awake()
+        {
+            var node = new LinkedListNode<LevelController>(firstLevel);
+            levels.AddFirst(node);
+        }
 
         public void SpawnNewLevel(GameObject trigger)
         {
             Vector3 position = new Vector3(trigger.transform.position.x, trigger.transform.position.y + levelHeight, trigger.transform.position.z);
             LevelController level = Instantiate(levelPrefabs[0], position, trigger.transform.rotation).GetComponent<LevelController>();
             level.spawnLevel.AddListener(SpawnNewLevel);
+
+            var node = new LinkedListNode<LevelController>(level);
+            levels.AddLast(node);
+
+            if (offsetToClean > 0) offsetToClean -= 1;
+            if (!needToDestroy) return;
+            DestroyHead();
+        }
+
+        private void DestroyHead()
+        {
+            var node = levels.First;
+
+            if(node != null)
+            {
+                Destroy(node.Value.gameObject);
+            }
+
+            levels.RemoveFirst();
         }
     }
 }
